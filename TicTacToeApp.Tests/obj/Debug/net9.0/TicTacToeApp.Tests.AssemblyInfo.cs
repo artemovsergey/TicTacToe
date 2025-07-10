7c9381a7f00b9354adb80e8319d58db90ad68b29
artemov <artik3314@gmail.com>
2025-07-10 22:49:25 +0300

    .

--- TicTacToeApp.Tests/obj/Debug/net9.0/TicTacToeApp.Tests.AssemblyInfo.cs
+++ TicTacToeApp.Tests/obj/Debug/net9.0/TicTacToeApp.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToeApp.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e00b5b60b4a5b206f1a3be6923b69dc70865294")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adb9abf89be7ce721ae9a258581025c1a9b3a85a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToeApp.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToeApp.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
