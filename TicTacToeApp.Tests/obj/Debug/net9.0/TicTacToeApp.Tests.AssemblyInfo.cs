3cd2320cbeed3aad1f5f7c44bca333962e4e7c95
artemov <artik3314@gmail.com>
2025-07-12 01:59:09 +0300

    .

--- TicTacToeApp.Tests/obj/Debug/net9.0/TicTacToeApp.Tests.AssemblyInfo.cs
+++ TicTacToeApp.Tests/obj/Debug/net9.0/TicTacToeApp.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToeApp.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+438674f57fa33846ebea05813873add1892ee118")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2d554ab4c4097fae17aa06b2bdca1f750368d3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToeApp.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToeApp.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- readme.md
+++ readme.md
@@ -7,3 +7,12 @@
  - добавить ct +
  - настроить openapi +
  - настроить валидацию  
+
+# Report of Coverage Test
+
+- dotnet tool install -g dotnet-reportgenerator-globaltool
+
+- установить `coverlet.msbuild`
+- dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=json
+
+- reportgenerator -reports:"coverage.json" -targetdir:"coveragereport" -reporttypes:TextSummary
