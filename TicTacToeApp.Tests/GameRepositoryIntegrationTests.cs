e187779d39f0fda66b609d2a5bcdd92fd0cec11f
artemov <artik3314@gmail.com>
2025-07-12 00:53:29 +0300

    ci cd pipeline

---
+++ TicTacToeApp.Tests/GameRepositoryIntegrationTests.cs
@@ -0,0 +1,64 @@
+using TicTacToeApp.API.Data;
+using TicTacToeApp.API.Entity;
+using TicTacToeApp.API.Repositories;
+
+namespace TicTacToeApp.Tests;
+
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Testcontainers.PostgreSql;
+using Xunit;
+
+public class GameRepositoryIntegrationTests : IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _postgreSqlContainer;
+    private TicTacToeContext _dbContext;
+    private GameAsyncRepository _repository;
+
+    public GameRepositoryIntegrationTests()
+    {
+        _postgreSqlContainer = new PostgreSqlBuilder()
+            .WithImage("postgres:latest")
+            .WithDatabase("testdb")
+            .WithUsername("postgres")
+            .WithPassword("postgres")
+            .Build();
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _postgreSqlContainer.StartAsync();
+        
+        var options = new DbContextOptionsBuilder<TicTacToeContext>()
+            .UseNpgsql(_postgreSqlContainer.GetConnectionString())
+            .Options;
+
+        _dbContext = new TicTacToeContext(options);
+        await _dbContext.Database.EnsureCreatedAsync();
+        
+        // Заполняем тестовыми данными
+        for (int i = 0; i < 5; i++)
+        {
+            _dbContext.Games.Add(new Game { Board = new string?[3][] });
+        }
+        await _dbContext.SaveChangesAsync();
+        
+        _repository = new GameAsyncRepository(_dbContext, null!);
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _dbContext.DisposeAsync();
+        await _postgreSqlContainer.DisposeAsync();
+    }
+
+    [Fact]
+    public async Task GetGamesAsync_ShouldReturn5GamesFromDatabase()
+    {
+        // Act
+        var games = await _repository.GetGamesAsync(CancellationToken.None);
+        
+        // Assert
+        Assert.Equal(5, games.Count());
+    }
+}
--- TicTacToeApp.Tests/obj/Debug/net9.0/TicTacToeApp.Tests.AssemblyInfo.cs
+++ TicTacToeApp.Tests/obj/Debug/net9.0/TicTacToeApp.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToeApp.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0bf6fbd9a5c98206ef5ee377cbeb00e549ed540")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+438674f57fa33846ebea05813873add1892ee118")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToeApp.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToeApp.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
