6d4e40bc4da495ffefc1e8cb482232734a8421d1
artemov <artik3314@gmail.com>
2025-07-13 19:18:52 +0300

    .

--- TicTacToeApp.API/Data/TicTacToeContext.cs
+++ TicTacToeApp.API/Data/TicTacToeContext.cs
@@ -9,7 +9,6 @@ public sealed class TicTacToeContext : DbContext
 {
     public TicTacToeContext(DbContextOptions<TicTacToeContext> opt) : base(opt)
     {
-        Database.EnsureDeleted();
         Database.Migrate();
     }
 
